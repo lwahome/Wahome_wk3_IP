0deb2d908886c157f28def93de22fb13f93e6a62
Lucy Wahome <LWahome@safaricom.co.ke>
2022-11-04 09:53:09 +0300

    comments on html added

--- js/main.js
+++ js/main.js
@@ -5,13 +5,14 @@ let total = 0;
 quizAnswers.forEach(function(jibu){
     if(parseInt(jibu.value) > 0) {
         total += parseInt(jibu.value);}
-        else{
+    else{
             total = total;}
-        jibu.addEventListener("change",function (event) {
+    jibu.addEventListener("change",function (event) {
             marks +=parseInt(event.target.value);
         });    
         });
         submitAnswer.addEventListener("click",function(event){
             let score = (marks/total)*100;
-        }
+            alert("Your score is" + score +"%");
+        });
     
